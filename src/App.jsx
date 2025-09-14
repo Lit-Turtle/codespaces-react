d4a42869dedf1c4b7b85d6766990300a8fd617cf
Kaipo <litturtleislandhello@gmail.com>
2025-09-14 18:15:28 +0000

    Declare values

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css';
 
 function App() {
+  //Delcaring properties
+  const wordArray = [];
+  const maxLetters = 5;
+  const [guess, setGuess] = useState("");
+  const [mainArr, setMainArr] = useState(() => Array(6).fill().map(() => Array(5).fill(" ")));
+
   return (
     <div className="App">
       
